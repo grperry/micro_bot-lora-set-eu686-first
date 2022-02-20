37152e7b615501f63bff6868841180ad6e1c3a59
grperry <92124381+grperry@users.noreply.github.com>
2022-02-20 17:24:51 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,10 @@ IotLoRaNode.InitialiseRadio(
 "AB0F43611CF64269FAFC263C2A521CC0",
 SpreadingFactors.Seven
 )
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    IotLoRaNode.DigitalValue(true, Channels.One)
+    IotLoRaNode.loraTransmitPayload()
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+})
