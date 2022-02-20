d2ea6eae0f46662b6a9d98cb22d4e481a62dfcf4
grperry <92124381+grperry@users.noreply.github.com>
2022-02-20 19:32:48 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,20 @@ SpreadingFactors.Seven
 )
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
     IotLoRaNode.DigitalValue(true, Channels.One)
+    IotLoRaNode.AnalogueValue(0, Channels.Two)
+    IotLoRaNode.TemperatureValue(input.temperature(), Channels.Three)
+    IotLoRaNode.AccelorometerValue(
+    input.acceleration(Dimension.X),
+    input.acceleration(Dimension.Y),
+    input.acceleration(Dimension.Z),
+    Channels.Four
+    )
+    IotLoRaNode.LightValue(input.lightLevel(), Channels.Five)
     IotLoRaNode.loraTransmitPayload()
+    for (let index = 0; index < 2; index++) {
+        basic.pause(5000)
+    }
     basic.showIcon(IconNames.Heart)
-    basic.pause(5000)
 })
